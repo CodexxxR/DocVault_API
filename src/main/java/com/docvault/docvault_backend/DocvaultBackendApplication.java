6e3dcdaa2de28e217164313b84d7ced2889e1a13
robin paul <paul.robin9024@gmail.com>
2025-08-02 19:43:22 +0530

    initial spring boot project setup

---
+++ src/main/java/com/docvault/docvault_backend/DocvaultBackendApplication.java
@@ -0,0 +1,13 @@
+package com.docvault.docvault_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DocvaultBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DocvaultBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/docvault/docvault_backend/DocvaultBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.docvault.docvault_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DocvaultBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
